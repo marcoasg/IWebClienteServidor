90c1eab6eb492edc2f79bb848e9f8b225de942a3
marcoasg <trigitos@uma.es>
2019-11-20 14:04:11 +0100

    Initializr

---
+++ src/test/java/com/olive/malagabici/IngWebClienteServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olive.malagabici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngWebClienteServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
