23eb16af1b78c7a457ae89d5150e741712c83d1c
marcoasg <trigitos@uma.es>
2019-12-05 17:28:37 +0100

    oautheado blodel

---
+++ src/main/java/com/olive/malagabici/controller/Controlador.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.olive.malagabici.controller;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author Trigi
+ */
+@Controller
+public class Controlador {
+
+    @RequestMapping("/")
+    public String doWelcome() {
+        return "login";
+    }
+
+    @PostMapping("/")
+    public String doLogin(@RequestParam(value = "id_token", required = true) String idToken, Model model) throws GeneralSecurityException, IOException {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())//???
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList("1019387256307-v6e72mosg75v9qq6bjb042pkbquq9hr5"))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+// (Receive idTokenString by HTTPS POST)
+        GoogleIdToken idTokenObj = verifier.verify(idToken);
+        if (idTokenObj != null) {
+            Payload payload = idTokenObj.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = payload.getEmailVerified();
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            // Use or store profile information
+            // ...
+            model.addAttribute("id_token", idToken);
+            return "index";
+        } else {
+            model.addAttribute("error", "Error en la autenticación, intente de nuevo.");
+            return "login";
+        }
+    }
+}
