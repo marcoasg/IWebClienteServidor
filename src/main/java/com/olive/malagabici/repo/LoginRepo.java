4fa2562b6c5972b487c09733a47add1bb9387798
marcoasg <trigitos@uma.es>
2019-12-07 01:26:32 +0100

    login FUNCIONANDO JODER, con registro automático y to me cago en dios

--- src/main/java/com/olive/malagabici/repo/LoginRepo.java
+++ src/main/java/com/olive/malagabici/repo/LoginRepo.java
@@ -5,29 +5,33 @@
  */
 package com.olive.malagabici.repo;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.web.client.RestTemplate;
+import org.apache.http.client.methods.HttpPost;
 
 /**
  *
  * @author Trigi
  */
-
 @Repository
 public class LoginRepo {
-    
-    private final static String URL_REGISTER_USER = "http://localhost:8081/webresources/entity.usuario/postByString";
-    
+
+    private final static String URL_REGISTER_USER = "http://localhost:8081/IngWebServiciosBD/webresources/entity.usuario/postByString";
+
     public void registerUser(String email, String name) {
         try {
-            List<String> params = new ArrayList<>();
-            params.add(email);
-            params.add(name);
+            String url = new StringBuilder(URL_REGISTER_USER)
+                    .append("?email=")
+                    .append(email)
+                    .append("&name=")
+                    .append(name)
+                    .toString();
 
-            RestTemplate restTemplate = new RestTemplate();
-            restTemplate.postForEntity(URL_REGISTER_USER, params, List.class);
+            HttpClient httpClient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(url);
+            httpClient.execute(httpPost);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
