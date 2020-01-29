a1820f79ca2905feb013148b83f9ac7767cd1e11
Cardenete <0619593375@uma.es>
2020-01-29 18:04:16 +0100

    WebSecurity cambiado

--- src/main/java/com/olive/malagabici/WebSecurityConfig.java
+++ src/main/java/com/olive/malagabici/WebSecurityConfig.java
@@ -5,9 +5,14 @@
  */
 package com.olive.malagabici;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.http.HttpHeaders.FROM;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
 
 /**
  *
@@ -23,5 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
       .requiresSecure();
     http.csrf().disable();
+    http.headers().addHeaderWriter(new XFrameOptionsHeaderWriter(new WhiteListedAllowFromStrategy(Arrays.asList("http://localhost/*"))));
   }
 }
